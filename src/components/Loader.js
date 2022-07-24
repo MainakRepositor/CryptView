6caf3290f01f2a05321472b20937ad78adba6cb7
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2022-07-24 19:42:01 +0530

    Create Loader.js

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const Loader = () => {
+    return (
+        <div>
+            <font color="white">
+                <center> Made by <a href="https://github.com/MainakRepositor">Mainak</a></center>
+            </font>
+        </div>
+    )
+}
+
+export default Loader
