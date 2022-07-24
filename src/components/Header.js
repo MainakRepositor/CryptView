63ff73f3737ce98ed7f95d8985a7455b31b7db32
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2022-07-24 17:28:30 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -62,7 +62,7 @@ function Header() {
             >
               <MenuItem value={"USD"}>USD</MenuItem>
               <MenuItem value={"INR"}>INR</MenuItem>
-              <MenuItem value={"EUR"}>INR</MenuItem>
+              <MenuItem value={"EUR"}>EUR</MenuItem>
             </Select>
           </Toolbar>
         </Container>
